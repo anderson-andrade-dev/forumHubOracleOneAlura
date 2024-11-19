549289ec912414c161b26aaf5e4a6cb33b3fccee
anderson-andrade-dev <147995158+anderson-andrade-dev@users.noreply.github.com>
2024-11-18 21:19:38 -0300

    Configuração do projeto

---
+++ src/main/java/br/com/canalandersonandrade/forumhuboracleonealura/ForumHubOracleOneAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.canalandersonandrade.forumhuboracleonealura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubOracleOneAluraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumHubOracleOneAluraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/canalandersonandrade/forumhuboracleonealura/ForumHubOracleOneAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.canalandersonandrade.forumhuboracleonealura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubOracleOneAluraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
